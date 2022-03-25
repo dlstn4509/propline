da83117f71483547221766516babff1eaebc0c10
dlstn4509 <dlstn7609@hanmail.net>
2022-03-25 17:52:16 +0900

    [Feat] last_login_ip, last_login_date 추가

--- t1/models/login/login.js
+++ t1/models/login/login.js
@@ -1,19 +1,32 @@
 const { pool } = require('../../modules/mysql-md');
 const bcrypt = require('bcrypt');
+const moment = require('moment');
 
 const login = async (query) => {
   try {
-    const { id, password } = query;
+    const { id, password, loginIp } = query;
+    let now = moment().format('YYYY-MM-DD HH:mm:ss');
     let sql = `
-      SELECT * FROM m001_member
-      WHERE member_id='${id}'
+      SELECT M.*, C.* FROM m001_member AS M
+      LEFT JOIN m002_company AS C ON M.cidx = C.cidx
+      WHERE M.member_id='${id}'
     `;
     const [data] = await pool.execute(sql);
     if (data.length === 0) {
       return { user: null, success: false };
     }
     let compare = await bcrypt.compare(password + process.env.BCRYPT_SALT, data[0].member_pw);
-    return compare ? { user: data[0], success: true } : { user: null, success: false };
+    if (compare) {
+      let sql = `
+        UPDATE m001_member 
+        SET last_login_ip='${loginIp}', last_login_date='${now}'
+        WHERE member_id='${id}'
+      `;
+      await pool.execute(sql);
+      return { user: data[0], success: true };
+    } else {
+      return { user: null, success: false };
+    }
   } catch (err) {
     throw new Error(err);
   }
