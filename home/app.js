5f5f300ccf18a09529e9bb8382e235349b89f8dd
dlstn4509 <dlstn7609@hanmail.net>
2022-08-09 09:55:17 +0900

    [feat] home

--- home/app.js
+++ home/app.js
@@ -8,11 +8,12 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 
 /************** view engine ***************/
 app.set('view engine', 'ejs');
-app.set('views', './t1/views');
+app.set('views', './home/views');
 app.locals.pretty = true;
 
 /*************** router init **************/
-app.get('/', (req, res, next) => {
+let paths = ['/main'];
+app.get(paths, (req, res, next) => {
   res.sendFile(path.join(__dirname, './public', 'index.html'));
 });
 
